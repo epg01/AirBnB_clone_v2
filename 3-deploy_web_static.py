24703e2dffa305f63f9d483fe1f98a776ad88147
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2021-01-22 18:19:31 +0000

    Hotfix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,52 +1,55 @@
 #!/usr/bin/python3
-# Full deploy create file tat to distribution send to server and
-# uncompress the file to get the static content
-
-from fabric.api import env, sudo, local, run, put
+"""
+Fabric script that distributes an archive to web servers
+"""
+from fabric.operations import put, run, sudo
+import os
+from fabric.api import run, local, sudo, env
 from datetime import datetime
-from os import path
-env.hosts = ["35.227.116.106", "34.74.163.244"]
-env.user = "ubuntu"
-
-
+dt = datetime.now()
+env.hosts = ['35.237.222.244', '34.73.185.36']
 def do_pack():
-    """ pack content files to .gz file"""
-    local("sudo mkdir -p versions")
-    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    name_file = "versions/web_static{}.tgz".format(date_time)
-    local("sudo tar -cvzf {} web_static".format(name_file))
-    return name_file
-
-
+    """ Packs web_static files into .tgz file
+    """
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + file_name + " ./web_static/")
+    if command == 0:
+        return file_name
+    return None
 def do_deploy(archive_path):
-    """ Deploy the .gz file to a host servers"""
-    if path.exists(archive_path) is False:
-        print("pass")
+    """ deploy an archive from the archive_path
+    """
+    if os.path.exists(archive_path) is False:
         return False
-
+    file_name = os.path.splitext(os.path.split(archive_path)[1])[0]
+    target = '/data/web_static/releases/' + file_name
+    path = archive_path.split('/')[1]
     try:
-        file_ = archive_path.split("/")[1]
-        filename = file_.split(".")[0]
+        """put("{}/tmp/".format(archive_path))
+        run('sudo mkdir -p {}'.format(target))
+        run('sudo tar -xzf /tmp/{} -C {}/'.format(path, target))
+        run('sudo rm /tmp/{}'.format(path))
+        run('sudo mv {}/web_static/* {}/'.format(target, target))
+        run('sudo rm -rf {}/web_static'.format(target))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}/ /data/web_static/current'.format(target))"""
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/" + filename)
-        run("sudo tar -zxf /tmp/{}.tgz -C {}".format(
-            filename, "/data/web_static/releases/" + filename))
-        run("sudo rm /tmp/{}".format(file_))
-        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static\
-/releases/{}'.format(filename, filename))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/{}\
-        /data/web_static/current".format(filename))
-        print("New version deployed!")
+        run('sudo mkdir -p ' + target)
+        run('sudo tar -xzf /tmp/' + path + ' -C ' + target + '/')
+        run('sudo rm /tmp/' + path)
+        run('sudo mv ' + target + '/web_static/* ' + target + '/')
+        run('sudo rm -rf ' + target + '/web_static')
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s ' + target + '/ /data/web_static/current')
         return True
     except:
         return False
-
-
 def deploy():
-    """ run the script to pack .gz file and deploy it """
-    archive_path = do_pack()
-    if archive_path is None:
-        print("pass")
+    """ make and ship static
+    """
+    path = do_pack()
+    if path is None:
         return False
-    return do_deploy(archive_path)
+    return do_deploy(path)
